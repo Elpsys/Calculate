f9c1f57c801a1233abc96582e67a3576c58e43b7
s20171103186 <s20171103186@admindeimac-34.local>
2018-10-12 20:05:28 +0800

    Initial Commit

---
+++ Calculate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculate
+//
+//  Created by s20171103186 on 2018/10/12.
+//  Copyright © 2018年 s20171103186. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
