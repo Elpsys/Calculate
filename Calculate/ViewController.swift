4f7ab4c8ee5e2df4602724a7b798c12224ba8900
s20171103186 <s20171103186@admindeiMac-34.local>
2018-11-16 21:08:54 +0800

    fix
    修复bug

--- Calculate/ViewController.swift
+++ Calculate/ViewController.swift
@@ -10,96 +10,237 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    self.image.layer.cornerRadius = avatarImage.frame.width / 2
-    self.image.layer.masksToBounds = true
+
     var temp:Double = 0
-    var Operatorflag = 0
+    var Operator:Int = 0
+    var temp1: Double = 0
+    var control = 0
     @IBOutlet weak var output: UITextField!
     @IBAction func add(_ sender: UIButton) {
-
-        temp = Double(output.text!)!
-        output.text = ""
-        Operatorflag = 1
-    }
-    @IBAction func subtraction(_ sender: UIButton) {
-        temp = Double(output.text!)!
-        output.text = ""
-        Operatorflag = 2
-    }
-    @IBAction func multip(_ sender: UIButton) {
-        temp = Double(output.text!)!
-        output.text = ""
-        Operatorflag = 3
-    }
-    @IBAction func division(_ sender: UIButton) {
-        temp = Double(output.text!)!
-        output.text = ""
-        Operatorflag = 4
-    }
-    @IBAction func clear(_ sender: UIButton) {
-        
-    }
-    @IBAction func equal(_ sender: UIButton) {
-        if Operatorflag == 1
+        if output.text == ""
+        {
+            output.text = "0"
+        }
+        else if output.text != "" && Operator == 1
         {
             temp = temp + Double(output.text!)!
             output.text = "\(temp)"
         }
-        if Operatorflag == 2
+        else if output.text != "" && Operator == 2
         {
             temp = temp - Double(output.text!)!
             output.text = "\(temp)"
         }
-        if Operatorflag == 3
+        else if output.text != "" && Operator == 3
         {
             temp = temp * Double(output.text!)!
             output.text = "\(temp)"
         }
-        if Operatorflag == 4
+        else if output.text != "" && Operator == 4
         {
             temp = temp / Double(output.text!)!
             output.text = "\(temp)"
         }
+        else
+        {
+            temp = Double(output.text!)!
+            output.text = ""
+            Operator = 1
+        }
+        temp1 = 0
+    }
+    @IBAction func subtraction(_ sender: UIButton) {
+        if output.text == ""
+        {
+            output.text = "0"
+        }
+        else
+        {
+            temp = Double(output.text!)!
+            output.text = ""
+            Operator = 2
+        }
+        temp1 = 0
+    }
+    @IBAction func multip(_ sender: UIButton) {
+        if output.text == ""
+        {
+            output.text = "0"
+        }
+        else
+        {
+            temp = Double(output.text!)!
+            output.text = ""
+            Operator = 3
+        }
+        temp1 = 0
+    }
+    @IBAction func division(_ sender: UIButton) {
+        if output.text == ""
+        {
+            output.text = "0"
+        }
+        else
+        {
+            temp = Double(output.text!)!
+            output.text = ""
+            Operator = 4
+        }
+        temp1 = 0
+    }
+    @IBAction func clear(_ sender: UIButton) {
+        output.text = ""
+        temp1 = 0
+        Operator = 0
+    }
+    @IBAction func equal(_ sender: UIButton) {
+        if Operator == 1
+        {
+            temp = temp + Double(output.text!)!
+            output.text = "\(temp)"
+        }
+        if Operator == 2
+        {
+            temp = temp - Double(output.text!)!
+            output.text = "\(temp)"
+        }
+        if Operator == 3
+        {
+            temp = temp * Double(output.text!)!
+            output.text = "\(temp)"
+        }
+        if Operator == 4
+        {
+            temp = temp / Double(output.text!)!
+            output.text = "\(temp)"
+        }
+        temp1 = 0
     }
     @IBAction func button(_ sender: UIButton){
-        output.text = output.text! + "1"
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "1"
+        }
+        else
+        {
+            output.text = output.text! + "1"
+        }
     }
     @IBAction func button2(_ sender: UIButton) {
-        output.text = output.text! + "2"
-   
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "2"
+        }
+        else
+        {
+            output.text = output.text! + "2"
+        }
     }
     @IBAction func button3(_ sender: UIButton) {
-        output.text = output.text! + "3"
-        
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "3"
+        }
+        else
+        {
+            output.text = output.text! + "3"
+        }
     }
     @IBAction func button4(_ sender: UIButton) {
-        output.text = output.text! + "4"
-        
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "4"
+        }
+        else
+        {
+            output.text = output.text! + "4"
+        }
     }
     @IBAction func button5(_ sender: UIButton) {
-        output.text = output.text! + "5"
-        
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "5"
+        }
+        else
+        {
+            output.text = output.text! + "5"
+        }
     }
     @IBAction func button6(_ sender: UIButton) {
-        output.text = output.text! + "6"
-        
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "6"
+        }
+        else
+        {
+            output.text = output.text! + "6"
+        }
     }
     @IBAction func button7(_ sender: UIButton) {
-        output.text = output.text! + "7"
-        
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "7"
+        }
+        else
+        {
+            output.text = output.text! + "7"
+        }
     }
     @IBAction func button8(_ sender: UIButton) {
-        output.text = output.text! + "8"
-        
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "8"
+        }
+        else
+        {
+            output.text = output.text! + "8"
+        }
     }
     @IBAction func button9(_ sender: Any) {
-        output.text = output.text! + "9"
-        
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "9"
+        }
+        else
+        {
+            output.text = output.text! + "9"
+        }
     }
     @IBAction func button0(_ sender: UIButton) {
-        output.text = output.text! + " 0"
-        
-        
+        if output.text == "0"
+        {
+            output.text = ""
+            output.text = output.text! + "0"
+        }
+        else
+        {
+            output.text = output.text! + "0"
+        }
+    }
+ 
+    @IBAction func point(_ sender: UIButton) {
+        if temp1 == 1
+        {
+            
+        }
+        else if output.text == ""
+        {
+            output.text = "0."
+        }
+        else
+        {
+            output.text = output.text! + "."
+            temp1 = 1
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
